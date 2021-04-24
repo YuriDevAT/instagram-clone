eaec886575ceaa2db1096411e4b27da4b08aa38b
YuriDevAT <JUndeutsch@gmx.at>
2021-04-24 20:16:58 +0200

    connected objects from seedfile with firestore

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,7 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+// import { seedDatabase } from '../seed';
 
 const config = {
     apiKey: "AIzaSyCTPUlo2OC6CpOthvCBY1pjG-q8Uh166y8",
@@ -14,5 +15,6 @@ const config = {
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
 
-console.log('firebase', firebase);
+// seedDatabase(firebase);
+
 export { firebase, FieldValue };
