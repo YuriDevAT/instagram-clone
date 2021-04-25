a408856f1a5e446b08331a3125e44906f14f7c44
YuriDevAT <JUndeutsch@gmx.at>
2021-04-25 12:43:48 +0200

    updated file with routes of new created pages

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 
 const Login = lazy(() => import ('./pages/login'));
+const SignUp = lazy(() => import ('./pages/signup'));
+const NotFound = lazy(() => import('./pages/not-found'));
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp}/>
+          <Route component={NotFound} />
         </Switch>
       </Suspense>
     </Router>
   );
 }
-
-export default App;
