dbf81796737705c0fa805dedc63ff67380373273
YuriDevAT <JUndeutsch@gmx.at>
2021-04-26 13:14:05 +0200

    created new function

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,3 +9,18 @@ export async function doesUsernameExist(username) {
 
     return result.docs.map((user) => user.data().length > 0);
 }
+
+export async function getUserByUserId(userId) {
+    const result = await firebase
+    .firestore()
+    .collection('users')
+    .where('userId', '==', userId)
+    .get();
+
+    const user = result.docs.map((item) => ({
+        ...item.data(),
+        docId: item.id
+    }));
+    
+    return user;
+}
