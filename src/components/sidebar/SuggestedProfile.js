94c534312a8ff34ed3e77b5dc0ba5ae6cbd823ca
YuriDevAT <JUndeutsch@gmx.at>
2021-04-27 16:32:54 +0200

    created suggestion profiles file for sidebar

---
+++ src/components/sidebar/SuggestedProfile.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { updateLoggedInUserFollowing, updateFollowedUserFollowers } from '../../services/firebase';
+
+export default function SuggestedProfile({ suggestedProfileDocId, username, profileId, userId }) {
+    const [followed, setFollowed] = useState(false);
+
+    async function handleFollowUser() {
+        setFollowed(true);
+
+        await updateLoggedInUserFollowing(loggedInUserDocId, profileId);
+
+        await updateFollowedUserFollowers(suggestedUserDocId, userId);
+
+    }
+    
+    return !followed ? (
+        <div className="flex flex-row items-center align-items justify-between">
+            <div className="flex items-center justify-between">
+                <img 
+                className="rounded-full w-8 flex mr-3"
+                src={`/images/avatars/${username}.png`}
+                alt=""
+                />
+                <Link to={`/p/${username}`}>
+                    <p className="font-bold text-sm">{username}</p>
+                </Link>
+            </div>
+            <button
+                className="text-xs font-bold text-blue-medium"
+                type="button"
+                onClick={handleFollowUser}>
+                    Follow
+            </button>
+        </div>
+    ) : null;
+}
+
+SuggestedProfile.propTypes = {
+    suggestedUserDocId: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    profileId: PropTypes.string.isRequired,
+    userId: PropTypes.string.isRequired,
+    loggedInUserDocId: PropTypes.string.isRequired
+}
