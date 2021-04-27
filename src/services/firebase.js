60c169418614a7f64c3d2740ce304652e1b7bc06
YuriDevAT <JUndeutsch@gmx.at>
2021-04-27 16:33:44 +0200

    created function for suggested profiles

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -24,3 +24,12 @@ export async function getUserByUserId(userId) {
     
     return user;
 }
+
+export async function getSuggestedProfiles(userId, following) {
+    const result = await firebase.firestore().collection('users').limit(10).get();
+
+    return result.docs
+    .map((user) => ({ ...user.data(), docId: user.id}))
+    .filter((profile) => profile.userId !== userId && !following.includes(profile.userId));
+}
+
