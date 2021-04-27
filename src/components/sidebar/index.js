f5eaa285308d85c0a596fb8d8bcea8f075a63048
YuriDevAT <JUndeutsch@gmx.at>
2021-04-27 16:34:09 +0200

    updated props for Suggestion Component

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,14 +1,15 @@
+import React from 'react';
 import useUser from '../../hooks/use-user';
 import User from './User';
 import Suggestions from './Suggestions';
 
 export default function Sidebar() {
-    const { user: { fullName, username, userId } } = useUser();
+    const { user: { docId, fullName, username, userId, following} } = useUser();
 
     return (
     <div className="p-4">
         <User username={username} fullName={fullName} />
-        <Suggestions userId={userId} />
+        <Suggestions userId={userId} following={following} loggedInUserdocId={docId} />
     </div>
     );
 }
