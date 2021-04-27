225e585d353ede0e256f8d150412a7e1c4b62515
YuriDevAT <JUndeutsch@gmx.at>
2021-04-27 16:33:23 +0200

    created component

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,3 +1,7 @@
 export default function Timeline() {
-    return <p>I am the timeline</p>;
+    return (
+    <div className="container col-span-2">
+
+    </div>
+    )
 }
--- src/components/sidebar/User.js
+++ src/components/sidebar/User.js
@@ -1,3 +1,33 @@
-export default function User() {
-    return <p>I am a user</p>;
-}
+import { memo } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
+
+const User = ({ username, fullName }) => {
+    !username || !fullName ? (
+        <Skeleton count={1} height={61} />
+    ) : (
+        <Link to={'/p/${username}'} className="grid grid-cols-4 gap-4 mb-6 items-center">
+        <div className="flex items-center justify-between col-span-1">
+            <img className="rounded-full w-16 flex mr-3"
+            src={`images/avatars/${username}.png`}
+            alt=""
+            />
+        </div>
+        
+        <div className="col-span-3">
+            <p className="font-bold text-sm">{username}</p>
+            <p className="text-sm">{fullName}</p>
+        </div>
+        </Link>
+    )
+};
+
+export default memo(User);
+
+User.propTypes = {
+    username: PropTypes.string,
+    fullName: PropTypes.string
+};
+
+User.whyDidYouRender = true;
