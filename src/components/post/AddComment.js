655e3de3a57e6ccf60c019ca99acd8f6af33dbe0
Julia Undeutsch <juliaundeutsch@Julias-MacBook-Air.local>
2021-05-16 13:35:00 -0500

    finished comment component

--- src/components/post/AddComment.js
+++ src/components/post/AddComment.js
@@ -13,8 +13,51 @@ export default function AddComment({ docId, comments, setComments, commentInput 
     const handleSubmitComment = (event) => {
         event.preventDefault();
 
-        return null;
+        setComments([{ displayName, comment}, ...comments])
+        setComment('');
+
+        return firebase
+        .firestore()
+        .collection('photos')
+        .doc(docId)
+        .update({
+            comments: FieldValue.arrayUnion({ displayName, comment })
+        });
     }
 
-    return null;
+    return (
+        <div className="border-t border-gray-primary">
+            <form
+            className="flex justify-between pl-0 pr-5"
+            method="POST"
+            onSubmit={(event) => comment.length >= 1 ? handleSubmitComment : event.preventDefault()}>
+                <input
+                aria-label="Add a comment"
+                autoComplete="off"
+                className="text-sm text-gray-base w-full mr-3 py-5 px-4"
+                type="text"
+                name="add-comment"
+                placeholder="Add a comment..."
+                value={comment}
+                onChange={({ target }) => setComment(target.value)}
+                ref={commentInput}
+                />
+                <button
+                className={`text-sm font-bold text-blue-medium ${!comment && 'opacity-25'}`}
+                type="button"
+                disabled={comment.length < 1}
+                onClick={handleSubmitComment}
+                >
+                Post
+                </button>
+            </form>
+        </div>
+    );
+}
+
+AddComment.propTypes = {
+    docId: PropTypes.string.isRequired,
+    comments: PropTypes.array.isRequired,
+    setComments: PropTypes.func.isRequired,
+    commentInput: PropTypes.object
 }
