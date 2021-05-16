5429591323052952e6c25556a976708f1579754a
Julia Undeutsch <juliaundeutsch@Julias-MacBook-Air.local>
2021-05-16 13:56:32 -0500

    created profile and updated routes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import * as ROUTES from './constants/routes';
 import UserContext from './context/user';
 import useAuthListener from './hooks/use-auth-listener';
 
+import ProtectedRoute from './helpers/protected-route';
+import IsUserLoggedIn from './helpers/is-user-logged-in';
+
 const Login = lazy(() => import ('./pages/login'));
 const SignUp = lazy(() => import ('./pages/signup'));
 const Dashboard = lazy(() => import ('./pages/dashboard'));
+const Profile = lazy(() => import ('./pages/profile'));
 const NotFound = lazy(() => import('./pages/not-found'));
 
 export default function App() {
@@ -17,9 +21,16 @@ export default function App() {
       <Router>
         <Suspense fallback={<p>Loading...</p>}>
           <Switch>
-            <Route path={ROUTES.LOGIN} component={Login} />
-            <Route path={ROUTES.SIGN_UP} component={SignUp}/>
-            <Route path={ROUTES.DASHBOARD} component={Dashboard}/>
+            <IsUserLoggedIn user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.LOGIN}>
+              <Login />
+            </IsUserLoggedIn>
+            <IsUserLoggedIn user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.SIGN_UP}>
+              <SignUp />
+            </IsUserLoggedIn>
+            <Route path ={ROUTES.PROFILE} component={Profile} />
+            <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
+            <Dashboard/>
+            </ProtectedRoute>
             <Route component={NotFound} />
           </Switch>
         </Suspense>
