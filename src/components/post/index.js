0872e16c97b81fea0610b57721e11f718aebcb78
Julia Undeutsch <juliaundeutsch@Julias-MacBook-Air.local>
2021-05-20 14:27:14 -0500

    created folder for img

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -2,42 +2,46 @@ import { useRef } from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header';
 import Image from './Image';
-import Action from './Actions';
+import Actions from './Actions';
 import Footer from './Footer';
 import Comments from './Comments';
 
 export default function Post({ content }) {
     const commentInput = useRef(null);
     const handleFocus = () => commentInput.current.focus();
-    
+  
+    // components
+    // -> header, image, actions (like & comment icons), footer, comments
     return (
-    <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
+      <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
         <Header username={content.username} />
-        <Image src={content.imageSrc} caption={content.caption}/>
-        <Action
-        docId={content.docId}
-        totalLikes={content.likes.length}
-        likedPhoto={content.userLikedPhoto}
-        handleFocus={handleFocus} />
+        <Image src={content.imageSrc} caption={content.caption} />
+        <Actions
+          docId={content.docId}
+          totalLikes={content.likes.length}
+          likedPhoto={content.userLikedPhoto}
+          handleFocus={handleFocus}
+        />
         <Footer caption={content.caption} username={content.username} />
         <Comments
-        docId={content.docId}
-        comments={content.comments}
-        posted={content.dateCreated}
-        commentInput={commentInput} />
-    </div>
-    )
-}
-
-Post.propTypes = {
+          docId={content.docId}
+          comments={content.comments}
+          posted={content.dateCreated}
+          commentInput={commentInput}
+        />
+      </div>
+    );
+  }
+  
+  Post.propTypes = {
     content: PropTypes.shape({
-        username: PropTypes.string.isRequired,
-        imageSrc: PropTypes.string.isRequired,
-        caption: PropTypes.string.isRequired,
-        docId: PropTypes.string.isRequired,
-        userLikedPhoto: PropTypes.bool.isRequired,
-        likes: PropTypes.array.isRequired,
-        comments: PropTypes.array.isRequired,
-        dateCreated: PropTypes.number.isRequired
+      username: PropTypes.string.isRequired,
+      imageSrc: PropTypes.string.isRequired,
+      caption: PropTypes.string.isRequired,
+      docId: PropTypes.string.isRequired,
+      userLikedPhoto: PropTypes.bool.isRequired,
+      likes: PropTypes.array.isRequired,
+      comments: PropTypes.array.isRequired,
+      dateCreated: PropTypes.number.isRequired
     })
-};
+  };
--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -2,21 +2,21 @@ import { useState, useEffect } from 'react';
 import { getPhotos } from '../services/firebase';
 
 export default function usePhotos(user) {
-    const [photos, setPhotos] = useState(null);
+  const [photos, setPhotos] = useState(null);
 
-    useEffect(() => {
-        async function getTimelinePhotos() {
-          // does the user actually follow people?
-          if (user?.following?.length > 0) {
-            const followedUserPhotos = await getPhotos(user.userId, user.following);
-            // re-arrange array to be newest photos first by dateCreated
-            followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
-            setPhotos(followedUserPhotos);
-          }
-        }
-    
-        getTimelinePhotos();
-      }, [user?.userId, user?.following]);
-    
-      return { photos };
+  useEffect(() => {
+    async function getTimelinePhotos() {
+      // does the user actually follow people?
+      if (user?.following?.length > 0) {
+        const followedUserPhotos = await getPhotos(user.userId, user.following);
+        // re-arrange array to be newest photos first by dateCreated
+        followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
+        setPhotos(followedUserPhotos);
+      }
     }
+
+    getTimelinePhotos();
+  }, [user?.userId, user?.following]);
+
+  return { photos };
+}
